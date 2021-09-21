5470579a854de039a8d41a18a6fb45f444b4e72d
Andrii Kleban <a.kleban@tenantcloud.com>
2021-09-21 10:15:06 +0300

    feature: Add api version config key. Add content type middleware for setting api version.

--- src/TenantCloud/RentlerSDK/Client/RentlerClientImpl.php
+++ src/TenantCloud/RentlerSDK/Client/RentlerClientImpl.php
@@ -4,8 +4,10 @@ namespace TenantCloud\RentlerSDK\Client;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
 use GuzzleHttp\RequestOptions;
 use Illuminate\Support\Facades\Config;
+use Psr\Http\Message\RequestInterface;
 use TenantCloud\GuzzleHelper\DumpRequestBody\HeaderObfuscator;
 use TenantCloud\GuzzleHelper\DumpRequestBody\JsonObfuscator;
 use TenantCloud\GuzzleHelper\GuzzleMiddleware;
@@ -51,10 +53,15 @@ class RentlerClientImpl implements RentlerClient
 		string $clientSecret,
 		TokenCache $cache
 	) {
+		$version = Config::get('rentler.version');
 		$this->authBaseUrl = $authBaseUrl;
 		$tokenResolver = new TokenResolver($this, $cache);
 
 		$stack = HandlerStack::create();
+
+		if ($version) {
+			$stack->push(Middleware::mapRequest(fn (RequestInterface $request) => $request->withHeader('Content-Type', "application/json; v={$version}")));
+		}
 
 		// Return all response body.
 		$stack->unshift(GuzzleMiddleware::fullErrorResponseBody());
