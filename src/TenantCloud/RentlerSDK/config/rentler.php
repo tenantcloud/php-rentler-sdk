d69fb53d118d1cf1fed8374b8d2a8644bbb8fd78
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2021-09-28 12:52:04 +0300

    misc: Rollback config default values

--- src/TenantCloud/RentlerSDK/config/rentler.php
+++ src/TenantCloud/RentlerSDK/config/rentler.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-	'base_url'      => env('RENTLER_PARTNER_BASE_URL', 'https://partner.reltner.com'),
-	'auth_base_url' => env('RENTLER_PARTNER_AUTH_BASE_URL', 'https://accounts.reltner.com'),
+	'base_url'      => env('RENTLER_PARTNER_BASE_URL', 'https://partner.rentler.com'),
+	'auth_base_url' => env('RENTLER_PARTNER_AUTH_BASE_URL', 'https://accounts.rentler.com'),
 	'scope'         => env('RENTLER_PARTNER_SCOPE', 'partner.rentler.com'),
 	'version'       => env('RENTLER_PARTNER_VERSION'),
 
-	'client_id'     => env('RENTLER_PARTNER_CLIENT_ID', 'tc'),
-	'client_secret' => env('RENTLER_PARTNER_CLIENT_SECRET', 'secret2'),
+	'client_id'     => env('RENTLER_PARTNER_CLIENT_ID', ''),
+	'client_secret' => env('RENTLER_PARTNER_CLIENT_SECRET', ''),
 	'timeout'       => env('RENTLER_PARTNER_CLIENT_TIMEOUT', 10),
 
 	'fake_client' => false,
