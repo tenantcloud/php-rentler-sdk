13414bc975692fa55292152611965fcb645123c3
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2022-06-07 19:44:41 +0300

    feature: New amenities

--- src/TenantCloud/RentlerSDK/Enums/ListingAmenity.php
+++ src/TenantCloud/RentlerSDK/Enums/ListingAmenity.php
@@ -48,6 +48,64 @@ class ListingAmenity extends ValueEnum
 
 	public static self $FENCED_YARD;
 
+	public static self $FURNISHED;
+
+	public static self $UNFURNISHED;
+
+	public static self $COMMUNITY_FITNESS;
+
+	public static self $COMMUNITY_TENNIS;
+
+	public static self $COMMUNITY_BASKETBALL;
+
+	public static self $COMMUNITY_VOLLEYBALL;
+
+	public static self $COMMUNITY_CLUBHOUSE;
+
+	public static self $COMMUNITY_BUSINESS_CENTER;
+
+	public static self $COMMUNITY_PLAYGROUND;
+
+	public static self $COMMUNITY_LAUNDRY;
+
+	public static self $COMMUNITY_POOL;
+
+	public static self $COMMUNITY_HOT_TUB;
+
+	public static self $COMMUNITY_PARK;
+
+	public static self $COMMUNITY_BBQ;
+
+	public static self $COMMUNITY_PET_WASHING_STATION;
+
+	public static self $COMMUNITY_DOG_PARK;
+
+	public static self $COMMUNITY_SPLASH_PAD;
+
+	public static self $COMMUNITY_GAME_ROOM;
+
+	public static self $COMMUNITY_THEATER_ROOM;
+
+	public static self $COMMUNITY_ELEVATOR_ACCESS;
+
+	public static self $COMMUNITY_CONTROLLED_ACCESS;
+
+	public static self $COMMUNITY_PUBLIC_TRANSIT;
+
+	public static self $COMMUNITY_WIFI_ACCESS;
+
+	public static self $COMMUNITY_ATTACHED_GARAGE;
+
+	public static self $COMMUNITY_BIKE_LOCKERS_STORAGE;
+
+	public static self $COMMUNITY_GARDEN;
+
+	public static self $COMMUNITY_PARCEL_PENDING;
+
+	public static self $COMMUNITY_FIRE_PITS;
+
+	public static self $COMMUNITY_INTERNET_CAFE;
+
 	/** @see self::GROUP_WASHERDRYER */
 	public static self $WASHER_DRYER_IN_UNIT;
 
@@ -126,6 +184,37 @@ class ListingAmenity extends ValueEnum
 		self::$POOL = new self('pool', 'Pool', self::GROUP_DEFAULT);
 		self::$DECK = new self('deck', 'Deck', self::GROUP_DEFAULT);
 		self::$FENCED_YARD = new self('fencedyard', 'Fenced yard', self::GROUP_DEFAULT);
+		self::$FURNISHED = new self('furnished', 'Furnished', self::GROUP_DEFAULT);
+		self::$UNFURNISHED = new self('unfurnished', 'Unfurnished', self::GROUP_DEFAULT);
+
+		// Community
+		self::$COMMUNITY_FITNESS = new self('fitness', 'Fitness Center', self::GROUP_DEFAULT);
+		self::$COMMUNITY_TENNIS = new self('tennis', 'Tennis Court', self::GROUP_DEFAULT);
+		self::$COMMUNITY_BASKETBALL = new self('basketball', 'Basketball Court', self::GROUP_DEFAULT);
+		self::$COMMUNITY_VOLLEYBALL = new self('volleyball', 'Volleyball Court', self::GROUP_DEFAULT);
+		self::$COMMUNITY_CLUBHOUSE = new self('clubhouse', 'Clubhouse', self::GROUP_DEFAULT);
+		self::$COMMUNITY_BUSINESS_CENTER = new self('businesscenter', 'Business Center', self::GROUP_DEFAULT);
+		self::$COMMUNITY_PLAYGROUND = new self('playground', 'Playground', self::GROUP_DEFAULT);
+		self::$COMMUNITY_LAUNDRY = new self('laundry', 'Onsite Laundry', self::GROUP_DEFAULT);
+		self::$COMMUNITY_POOL = new self('pool', 'Pool', self::GROUP_DEFAULT);
+		self::$COMMUNITY_HOT_TUB = new self('hottub', 'Hot Tub', self::GROUP_DEFAULT);
+		self::$COMMUNITY_PARK = new self('park', 'Near Park', self::GROUP_DEFAULT);
+		self::$COMMUNITY_BBQ = new self('bbq', 'Bbq Area', self::GROUP_DEFAULT);
+		self::$COMMUNITY_PET_WASHING_STATION = new self('petwashingstation', 'Pet Washing Station', self::GROUP_DEFAULT);
+		self::$COMMUNITY_DOG_PARK = new self('dogpark', 'Dog Park', self::GROUP_DEFAULT);
+		self::$COMMUNITY_SPLASH_PAD = new self('splashpad', 'Splash Pad', self::GROUP_DEFAULT);
+		self::$COMMUNITY_GAME_ROOM = new self('gameroom', 'Game Room', self::GROUP_DEFAULT);
+		self::$COMMUNITY_THEATER_ROOM = new self('theaterroom', 'Theater Room', self::GROUP_DEFAULT);
+		self::$COMMUNITY_ELEVATOR_ACCESS = new self('elevatoraccess', 'Elevator Access', self::GROUP_DEFAULT);
+		self::$COMMUNITY_CONTROLLED_ACCESS = new self('controlledaccess', 'Controlled Access', self::GROUP_DEFAULT);
+		self::$COMMUNITY_PUBLIC_TRANSIT = new self('publictransit', 'Public Transit', self::GROUP_DEFAULT);
+		self::$COMMUNITY_WIFI_ACCESS = new self('wifiaccess', 'WiFi in Common Areas', self::GROUP_DEFAULT);
+		self::$COMMUNITY_ATTACHED_GARAGE = new self('attachedgarage', 'Attached Garage', self::GROUP_DEFAULT);
+		self::$COMMUNITY_BIKE_LOCKERS_STORAGE = new self('bikelockersstorage', 'Bike Lockers/Storage', self::GROUP_DEFAULT);
+		self::$COMMUNITY_GARDEN = new self('garden', 'Community Garden', self::GROUP_DEFAULT);
+		self::$COMMUNITY_PARCEL_PENDING = new self('parcelpending', 'Parcel Pending', self::GROUP_DEFAULT);
+		self::$COMMUNITY_FIRE_PITS = new self('firepits', 'Fire Pits', self::GROUP_DEFAULT);
+		self::$COMMUNITY_INTERNET_CAFE = new self('internetcafe', 'Internet Cafe', self::GROUP_DEFAULT);
 
 		// washerdryer
 		self::$WASHER_DRYER_IN_UNIT = new self('washerdryerinunit', 'In Unit', self::GROUP_WASHERDRYER);
