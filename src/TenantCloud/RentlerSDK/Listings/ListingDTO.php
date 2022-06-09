1be79fa29df6ee504bcd2487e91a894d64b5c41e
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2022-06-09 16:42:52 +0300

    feature: New listing fields

--- src/TenantCloud/RentlerSDK/Listings/ListingDTO.php
+++ src/TenantCloud/RentlerSDK/Listings/ListingDTO.php
@@ -148,6 +148,9 @@ use TenantCloud\RentlerSDK\SyndicationProviders\SyndicationProviderDTO;
  * @method self                          setAllowCats(?bool $allowCats)
  * @method bool|null                     getAllowCats()
  * @method bool                          hasAllowCats()
+ * @method self                          setAllowOtherPets(?bool $allowOtherPets)
+ * @method bool|null                     getAllowOtherPets()
+ * @method bool                          hasAllowOtherPets()
  * @method self                          setAllowSmoking(?bool $allowSmoking)
  * @method bool|null                     getAllowSmoking()
  * @method bool                          hasAllowSmoking()
@@ -299,6 +302,7 @@ class ListingDTO extends CamelDataTransferObject
 		'allowLargeDogs',
 		'breedRestrictions',
 		'allowCats',
+		'allowOtherPets',
 		'allowSmoking',
 		'smokingDescription',
 		'communityTitle',
@@ -330,6 +334,8 @@ class ListingDTO extends CamelDataTransferObject
 		'isApplicationsEnabled',
 		'currencyCode',
 		'isVerified',
+		'rentSchedule',
+		'rentStyle',
 	];
 
 	public function setCoordinates(?array $coordinates): self
@@ -441,5 +447,21 @@ class ListingDTO extends CamelDataTransferObject
 		$result = array_map(fn ($item) => OperatingDayDTO::from($item), $operatingHours);
 
 		return $this->set('operatingHours', $result);
+	}
+
+	/**
+	 * @param string|ListingRentSchedule|null $rentSchedule
+	 */
+	public function setRentSchedule($rentSchedule): self
+	{
+		return $this->set('rentSchedule', $rentSchedule ? ListingRentSchedule::fromValue($rentSchedule) : null);
+	}
+
+	/**
+	 * @param string|ListingRentStyle|null $rentStyle
+	 */
+	public function setRentStyle($rentStyle): self
+	{
+		return $this->set('rentSchedule', $rentStyle ? ListingRentStyle::fromValue($rentStyle) : null);
 	}
 }
