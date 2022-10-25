0025502f6132e0402c6f751691b6628d907b238d
Andrii Kleban <a.kleban@tenantcloud.com>
2022-10-25 18:38:21 +0300

    refactor: Add extend annotation for enums.

--- src/TenantCloud/RentlerSDK/Enums/ScreeningStatus.php
+++ src/TenantCloud/RentlerSDK/Enums/ScreeningStatus.php
@@ -4,6 +4,9 @@ namespace TenantCloud\RentlerSDK\Enums;
 
 use TenantCloud\Standard\Enum\ValueEnum;
 
+/**
+ * @extends ValueEnum<string>
+ */
 class ScreeningStatus extends ValueEnum
 {
 	public static self $IDENTITY_VERIFICATION_PENDING;
